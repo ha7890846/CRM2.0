a3dd24a5d2bcd7fe19b756c1316280979e233893
ha7890846 <ha7890846@gmail.com>
2025-06-28 11:05:15 +0530

    add content to emp Profile

--- Frontend/src/component/EmpProfile.jsx
+++ Frontend/src/component/EmpProfile.jsx
@@ -1,7 +1,22 @@
+import style from "../styling/EmpProfile.module.css";
 const EmpProfile = () => {
     return (
         <>
-            Hello Profiel
+            <div className={style.mainContainer}>
+                <form action="">
+                    <label htmlFor="">First Name</label>
+                    <input type="text" name="" id="" />
+                    <label htmlFor="">Last Name</label>
+                    <input type="text" name="" id="" />
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="" id="" />
+                    <label htmlFor="">Password</label>
+                    <input type="password" name="" id="" />
+                    <label htmlFor="">Verify Password</label>
+                    <input type="password" name="" id="" />
+                    <button>save</button>
+                </form>
+            </div>
         </>
     );
 };
