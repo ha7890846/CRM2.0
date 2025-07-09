da18b98a70b01a26955725409e908cf81f5c8c32
ha7890846 <ha7890846@gmail.com>
2025-07-09 11:11:53 +0530

    fix error

--- Backend/routes/empRouter.js
+++ Backend/routes/empRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getEmp, addEmp } = require("../controllers/empController");
+const { getEmp, addEmp,deleteEmp } = require("../controllers/empController");
 const router = express.Router();
 
 router.get("/",getEmp);
 router.post("/add",addEmp);
+router.delete("/:id", deleteEmp);
 module.exports= router;
